c5ea2e91a1ccc1193c4423607028651c2571c109
Bikrant <bikrantthakur6@gmail.com>
2022-01-12 19:13:50 +0530

    added functions and comments

--- Linkedlist/SinglyLinkedList.cpp
+++ Linkedlist/SinglyLinkedList.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
 
+#define MININT -32768
+#define MAXINT 32767
 using namespace std;
 
+
+//structure of linkedlist
 struct node{
     int data;
     node *next;
 }*head,*temp;
 
+
+//1.function to insert elements into linkedlist
 
 void insert(int val){
     node *newnode=new node;
@@ -24,6 +30,7 @@ void insert(int val){
     }
 }
 
+//2.function to display elements of linkedlist
 
 void display(node *np){
     while(np!=NULL){
@@ -31,6 +38,8 @@ void display(node *np){
         np=np->next;
     }
 }
+
+//3.function to count elements of linkedlist
 
 int count(node *np){
     int c=0;
@@ -42,6 +51,7 @@ int count(node *np){
 }
 
 
+//4.recursive function to count elements of linkedlist
 
 int count_recursive(node *np){
     int c=0;
@@ -51,6 +61,8 @@ int count_recursive(node *np){
         return count_recursive(np->next)+1;
     }
 }
+
+//5.function to add elements of linkedlist
 
 int add(node *np){
     int sum=0;
@@ -62,6 +74,8 @@ int add(node *np){
     return sum;
 }
 
+//6.recursive function to add elements of  linkedlist
+
 int add_recursive(node *np){
     int sum=0;
     if(np==NULL){
@@ -71,6 +85,53 @@ int add_recursive(node *np){
     }
     return sum;
 }
+
+//7.function to find largest of all elements of linkedlist
+int max(node *np){
+    int m=MININT;
+    while(np){
+        if(np->data>m)
+            m=np->data;
+        np=np->next;
+    }
+    return m;
+}
+
+//8. recursive function to find largest of all elements of linkedlist
+
+int max_recursive(node *np){
+    int m;
+    if(np==0)
+        return MININT;
+    m=max_recursive(np->next);
+    return m>np->data?m:np->data;
+}
+
+
+//9.function to find smallest of all elements of linkedlist
+
+int min(node *np){
+    int m=MAXINT;
+    while(np){
+        if(np->data<m)
+            m=np->data;
+        np=np->next;
+    }
+    return m;
+}
+
+//10.recursive function to find smallest of all elements of linkedlist
+
+int min_recursive(node *np){
+    int m;
+    if(np==0)
+        return MAXINT;
+    m=min_recursive(np->next);
+    return m<np->data?m:np->data;
+}
+
+
+//main function
 
 int main(){
     head=NULL;
@@ -84,8 +145,12 @@ int main(){
     display(head);
     cout<<"\n";
     cout<<"Total number of element is:"<<count(head)<<endl;
-    cout<<"Total number of element recursively is:"<<count_recursive(head)<<endl;
+    cout<<"Using recursion,total number of element is:"<<count_recursive(head)<<endl;
     cout<<"Total sum of all element is:"<<add(head)<<endl;
-    cout<<"Total sum of all element using recursion  is:"<<add_recursive(head)<<endl;
+    cout<<"Using recursion,total sum of all element is:"<<add_recursive(head)<<endl;
+    cout<<"The maximum of all numbers is:"<<max(head)<<endl;
+    cout<<"Using recursion,the maximum of all numbers is:"<<max_recursive(head)<<endl;
+    cout<<"The minimum of all numbers is:"<<min(head)<<endl;
+    cout<<"Using recursion,the minimum of all numbers is:"<<min_recursive(head)<<endl;
     return 0;
 }
